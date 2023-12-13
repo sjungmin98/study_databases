f5fb3572e52a33d12553682e7e97ea638f8dc60e
sjungmin98 <shc980@gmail.com>
2023-12-13 17:41:06 +0900

    first commit

---
+++ docs/NoSQL/mongo_inserts.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient
+# mongodb에 접속 -> 자원에 대한 class
+mongoClient = MongoClient("mongodb://localhost:27017")
+
+# database 연결
+database = mongoClient["local"]
+
+# collection 작업
+collection = database['fruits']
+
+# insert 작업 진행
+collection
+
+pass
