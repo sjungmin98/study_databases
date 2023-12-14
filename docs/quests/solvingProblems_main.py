bbd4ab98678e2cd4ff3a6a5621f9175b05c8f40d
sjungmin98 <60747339+sjungmin98@users.noreply.github.com>
2023-12-15 01:56:42 +0900

    solvingProblems_main.py

--- docs/quests/solvingProblems_main.py
+++ docs/quests/solvingProblems_main.py
@@ -1,69 +1,74 @@
-# MongoClient 모듈 불러오기
-from pymongo import MongoClient
+# solvingProblems_main 모듈에서 connect_to_mongodb와 insert_quiz_list 함수를 가져옴
+from solvingProblems_main import connect_to_mongodb, insert_quiz_list
 
-# 데이터베이스 및 컬렉션 이름을 설정
-database_name = 'local'
-collection_name = 'solvingproblem'
+# MongoDB에서 문제와 선택지를 가져와서 사용자에게 문제를 출제하고 답을 받는 함수
+def question_choices_from_mongodb(collection):
+    user_name = input("이름을 입력해 주세요: ")  # 사용자 이름 입력 받기
 
-# MongoDB에 연결하고 지정된 데이터베이스와 컬렉션에 연결하는 함수를 정의
-def connect_to_mongodb(database_name, collection_name):
-    # MongoClient를 사용하여 로컬호스트의 MongoDB에 연결
-    client = MongoClient('mongodb://localhost:27017/')
-    # 지정된 데이터베이스를 선택
-    database = client[database_name]
-    # 지정된 컬렉션을 선택
-    collection = database[collection_name]
-    # 컬렉션 객체를 반환
-    return collection
+    query = {}  # MongoDB에서 모든 문서를 선택하기 위한 빈 쿼리(딕셔너리) 생성
+    
+    # 필요한 필드만 선택하기 위한 프로젝션 생성
+    # 프로젝션 : 한 document 전체를 선택하는 것이 아니라, 특정 필요한 데이터를 선택하는 것
+    projection = {'question': 1, 'choices': 1, 'answer_number': 1, '_id': 1}  
+                                                                            
+    # 쿼리와 프로젝션을 사용하여 MongoDB에서 데이터를 가져옴
+    result = collection.find(query, projection)
+    result = list(result)  # 결과를 리스트로 변환
 
-# 퀴즈 데이터를 MongoDB에 삽입하는 함수를 정의
-def insert_quiz_list(collection, quiz_list):
-    for quiz in quiz_list:
-        # 동일한 질문을 가진 문서가 존재하지 않는 경우에만 삽입
-        if collection.count_documents({"question": quiz["question"]}) == 0:
-            # 퀴즈를 컬렉션에 삽입
-            collection.insert_one(quiz)
+    question_number = 1  # 문제 번호 초기화
+    correct_answers = 0  # 정답 개수 초기화
 
-# MongoDB에 연결하고 지정된 데이터베이스와 컬렉션에 연결
+    # 각 문서에 대해
+    for document in result:
+        question = document['question']  # 문제를 가져옴
+        choices = document['choices']  # 선택지를 가져옴
+
+        # 문제를 출력
+        print("문제{}. {}".format(question_number, question))
+
+        # 각 보기에 대해
+        choice_number = 1  # 보기 번호 초기화
+        for choice in choices:
+            # 보기를 출력
+            print("{}. {}".format(choice_number, choice))
+            choice_number += 1  # 선택지 번호를 하나 증가시킴
+
+        # 사용자로부터 답을 입력 받음
+        user_answer = int(input("답을 입력하세요 : "))
+
+        # 사용자의 답이 정답이면
+        if user_answer == document['answer_number']:
+            print("정답입니다!")
+            correct_answers += 1  # 정답 개수를 하나 증가시킴
+        else:
+            print("틀렸습니다!")
+
+        # MongoDB에 사용자의 이름과 답을 저장
+        #  $set은 MongoDB의 update 연산자 중 하나로, 특정 필드의 값을 변경하거나 새 필드를 추가할 때 사용
+        #  _id: MongoDB에서 document는 고유한 _id 필드 자동으로 생성, 이 _id 필드를 사용하면 특정 문서를 쉽게 찾을 수 있음.
+        
+        collection.update_one({'_id': document['_id']}, {"$set": {user_name: user_answer}})
+
+        question_number += 1  # 문제 번호를 하나 증가시킴
+
+    return correct_answers  # 정답 개수를 반환
+
+# 정답 개수에 따라 최종 점수를 계산하는 함수
+def calculate_final_score(correct_answers):
+    final_score = correct_answers * 20  # 정답 개수에 20을 곱하여 점수를 계산
+    if final_score > 100:  # 점수가 100을 초과하면
+        final_score = 100  # 점수를 100으로 조정
+    return final_score  # 최종 점수를 반환
+
+# MongoDB에 연결하고, 문제 리스트를 가져옴
 collection = connect_to_mongodb(database_name, collection_name)
+quiz_list = insert_quiz_list(collection)
 
-# 삽입할 퀴즈 데이터를 정의
-quiz_list = [
-    {
-        "question": "Python의 생성자 함수 이름은 무엇인가요?",
-        "choices": ["__init__", "__main__", "__str__", "__del__"],
-        "answer": "__init__",
-        "answer_number": 1,
-        "score": 20
-    },
-    {
-        "question": "Python에서 'Hello, World!'를 출력하는 코드는 무엇인가요?",
-        "choices": ["print('Hello, World!')", "console.log('Hello, World!')", "printf('Hello, World!')", "echo 'Hello, World!'"],
-        "answer": "print('Hello, World!')",
-        "answer_number": 1,
-        "score": 20
-    },
-    {
-        "question": "Python에서 주석을 나타내는 기호는 무엇인가요?",
-        "choices": ["//", "/* */", "#", "--"],
-        "answer": "#",
-        "answer_number": 3,
-        "score": 20
-    },
-    {
-        "question": "Python에서 리스트의 길이를 반환하는 함수는 무엇인가요?",
-        "choices": ["size()", "length()", "len()", "sizeof()"],
-        "answer": "len()",
-        "answer_number": 3,
-        "score": 20
-    },
-    {
-        "question": "Python에서 문자열을 숫자로 변환하는 함수는 무엇인가요?",
-        "choices": ["str()", "int()", "char()", "float()"],
-        "answer": "int()",
-        "answer_number": 2,
-        "score": 20
-    }
-]
+# 문제를 출제하고, 사용자의 답을 받아 정답 개수를 계산
+correct_answers = question_choices_from_mongodb(collection)
 
-insert_quiz_list(collection, quiz_list)
+# 최종 점수를 계산
+final_score = calculate_final_score(correct_answers)
+
+# 최종 점수를 출력
+print("최종 점수 : {}".format(final_score))
