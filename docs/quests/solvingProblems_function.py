1942d14fd8006c1d321aea99e63cbf4d9b308804
sjungmin98 <shc980@gmail.com>
2023-12-14 23:32:27 +0900

    so hard

--- docs/quests/solvingProblems_function.py
+++ docs/quests/solvingProblems_function.py
@@ -1,64 +1,62 @@
-# fruit_info_list = [
-#     {"name": "Apple", "color": "Red", "origin": "Asia"},
-#     {"name": "Banana", "color": "Yellow", "origin": "Southeast Asia"},
-#     {"name": "Orange", "color": "Orange", "origin": "Southeast Asia"},
-#     {"name": "Grapes", "color": "Purple", "origin": "Europe"}]
+from solvingProblems_main import connect_to_mongodb
 
-# Connect to MongoDB
-def connect_mongo(link, database_name, collection_name): 
-    from pymongo import MongoClient
-    mongoclient = MongoClient(link)
-    database = mongoclient[database_name]  
-    return database[collection_name]  
+def question_choices_from_mongodb(collection, max_questions=5):
+    # MongoDB에서 문제와 선택지를 가져오는 함수
+    # 쿼리 작성
+    query = {}
 
-def input_quiz(quiz):
-    collection.insert_one(quiz)
+    # 프로젝션 작성
+    projection = {'question': 1, 'choices': 1, 'answer_number': 1, 'user_answer': 1, '_id': 0}
 
+    # 쿼리 실행
+    result = collection.find(query, projection)
+    result = list(result)  
 
-quiz_list = [
-    {
-        "question": "Python의 생성자 함수 이름은 무엇인가요?",
-        "choices": ["__init__", "__main__", "__str__", "__del__"],
-        "answer": "__init__",
-        "answer_number": 1,
-        "score": 20
-    },
-    {
-        "question": "Python에서 'Hello, World!'를 출력하는 코드는 무엇인가요?",
-        "choices": ["print('Hello, World!')", "console.log('Hello, World!')", "printf('Hello, World!')", "echo 'Hello, World!'"],
-        "answer": "print('Hello, World!')",
-        "answer_number": 1,
-        "score": 20
-    },
-    {
-        "question": "Python의 주석을 나타내는 기호는 무엇인가요?",
-        "choices": ["//", "/* */", "#", "--"],
-        "answer": "#",
-        "answer_number": 3,
-        "score": 20
-    },
-    {
-        "question": "Python에서 리스트의 길이를 반환하는 함수는 무엇인가요?",
-        "choices": ["size()", "length()", "len()", "sizeof()"],
-        "answer": "len()",
-        "answer_number": 3,
-        "score": 20
-    },
-    {
-        "question": "Python에서 문자열을 숫자로 변환하는 함수는 무엇인가요?",
-        "choices": ["str()", "int()", "char()", "float()"],
-        "answer": "int()",
-        "answer_number": 2,
-        "score": 20
-    }
-]
+    # 결과 출력
+    question_number = 1
+    correct_answers = 0  # 정답 개수 초기화
 
+    for document in result:
+        # 문제 출력
+        print("문제{}. {}".format(question_number, document['question']))
+        
+        # 선택지 출력
+        choice_number = 1
+        for choice in document['choices']:
+            print("{}. {}".format(choice_number, choice))
+            choice_number += 1
+        
+        # 사용자 입력 받기
+        user_answer = input("답을 입력하세요 : ")
 
+        # 정답 확인
+        if 'answer_number' in document and int(user_answer) == document['answer_number'] :
+            print("정답입니다!")
+            correct_answers += 1
+        else:
+            print("틀렸습니다!")
 
-collection = connect_mongo("mongodb://localhost:27017", "local", "solvingproblem")
+        question_number += 1
 
-# 각각의 정보를 mongoDB로 insert
-for quiz in [0, 1, 2, 3, 4]:
-    quiz_info_list = quiz_list[quiz]
-    input_quiz(quiz_info_list)
-pass
+        # 일정 개수의 문제를 출제하고 종료
+        if question_number > max_questions :
+            break
+
+    return correct_answers
+
+def calculate_final_score(correct_answers) :
+    # 정답 개수에 따라 최종 점수 계산
+    final_score = correct_answers * 20
+    if final_score > 100 :
+        final_score = 100
+    return final_score
+
+# 문제와 답안 출력 및 사용자 입력
+database_name = 'local'
+collection_name = 'solvingproblem'
+collection = connect_to_mongodb(database_name, collection_name)
+correct_answers = question_choices_from_mongodb(collection, max_questions=5)
+
+# 최종 점수 계산 및 출력
+final_score = calculate_final_score(correct_answers)
+print("최종 점수 : {}".format(final_score))
--- docs/quests/solvingProblems_main.py
+++ docs/quests/solvingProblems_main.py
@@ -1,42 +1,69 @@
+from pymongo import MongoClient
+
+# 기본 데이터베이스와 컬렉션 이름
+database_name = 'local'
+collection_name = 'solvingproblem'
+
+def connect_to_mongodb(database_name, collection_name) :
+    # MongoDB에 연결하는 함수
+    # MongoClient를 이용해 MongoDB 서버에 접속
+    client = MongoClient('mongodb://localhost:27017/') 
+
+    # 데이터베이스 선택
+    database = client[database_name]
+    
+    # 컬렉션 선택 (없으면 새로 생성)
+    collection = database[collection_name]
+
+    return collection
+
+def insert_quiz_data(collection, quiz_list) :
+    # MongoDB에 데이터를 삽입하는 함수
+    # insert_many 메서드를 사용하여 여러 문서를 한 번에 삽입
+    collection.insert_many(quiz_list)
+
+# MongoDB에 연결
+# 기본적으로 'local' 데이터베이스, 'solvingproblem' 컬렉션을 사용
+collection = connect_to_mongodb(database_name, collection_name)
+
+# 삽입할 퀴즈 데이터
 quiz_list = [
     {
         "question": "Python의 생성자 함수 이름은 무엇인가요?",
         "choices": ["__init__", "__main__", "__str__", "__del__"],
         "answer": "__init__",
+        "answer_number": 1,
         "score": 20
     },
     {
         "question": "Python에서 'Hello, World!'를 출력하는 코드는 무엇인가요?",
         "choices": ["print('Hello, World!')", "console.log('Hello, World!')", "printf('Hello, World!')", "echo 'Hello, World!'"],
         "answer": "print('Hello, World!')",
+        "answer_number": 1,
         "score": 20
     },
     {
-        "question": "Python의 주석을 나타내는 기호는 무엇인가요?",
+        "question": "Python에서 주석을 나타내는 기호는 무엇인가요?",
         "choices": ["//", "/* */", "#", "--"],
         "answer": "#",
+        "answer_number": 3,
         "score": 20
     },
     {
         "question": "Python에서 리스트의 길이를 반환하는 함수는 무엇인가요?",
         "choices": ["size()", "length()", "len()", "sizeof()"],
         "answer": "len()",
+        "answer_number": 3,
         "score": 20
     },
     {
         "question": "Python에서 문자열을 숫자로 변환하는 함수는 무엇인가요?",
         "choices": ["str()", "int()", "char()", "float()"],
         "answer": "int()",
+        "answer_number": 2,
         "score": 20
     }
 ]
 
-for index in [range(len(quiz_list))]:
-    quiz = quiz_list[index]
-    print("{}. {}".format(index + 1, quiz['question']))
-    for i in range(len(quiz['choices'])):
-        choice = quiz['choices'][i]
-        print("   {}. {}".format(i + 1, choice))
-    print()
-
-
+# 퀴즈 데이터 삽입
+insert_quiz_data(collection, quiz_list)
