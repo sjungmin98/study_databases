4778778f201a8115de5a14b0c3b01ef6c60f48a3
sjungmin98 <60747339+sjungmin98@users.noreply.github.com>
2023-12-15 02:10:38 +0900

    solvingProblems_function.py

--- docs/quests/solvingProblems_function.py
+++ docs/quests/solvingProblems_function.py
@@ -1,62 +1,71 @@
-from solvingProblems_main import connect_to_mongodb
+# MongoClient 모듈을 import
+from pymongo import MongoClient
 
-def question_choices_from_mongodb(collection, max_questions=5):
-    # MongoDB에서 문제와 선택지를 가져오는 함수
-    # 쿼리 작성
-    query = {}
-
-    # 프로젝션 작성
-    projection = {'question': 1, 'choices': 1, 'answer_number': 1, 'user_answer': 1, '_id': 0}
-
-    # 쿼리 실행
-    result = collection.find(query, projection)
-    result = list(result)  
-
-    # 결과 출력
-    question_number = 1
-    correct_answers = 0  # 정답 개수 초기화
-
-    for document in result:
-        # 문제 출력
-        print("문제{}. {}".format(question_number, document['question']))
-        
-        # 선택지 출력
-        choice_number = 1
-        for choice in document['choices']:
-            print("{}. {}".format(choice_number, choice))
-            choice_number += 1
-        
-        # 사용자 입력 받기
-        user_answer = input("답을 입력하세요 : ")
-
-        # 정답 확인
-        if 'answer_number' in document and int(user_answer) == document['answer_number'] :
-            print("정답입니다!")
-            correct_answers += 1
-        else:
-            print("틀렸습니다!")
-
-        question_number += 1
-
-        # 일정 개수의 문제를 출제하고 종료
-        if question_number > max_questions :
-            break
-
-    return correct_answers
-
-def calculate_final_score(correct_answers) :
-    # 정답 개수에 따라 최종 점수 계산
-    final_score = correct_answers * 20
-    if final_score > 100 :
-        final_score = 100
-    return final_score
-
-# 문제와 답안 출력 및 사용자 입력
+# 데이터베이스 및 컬렉션 이름을 설정
 database_name = 'local'
 collection_name = 'solvingproblem'
-collection = connect_to_mongodb(database_name, collection_name)
-correct_answers = question_choices_from_mongodb(collection, max_questions=5)
 
-# 최종 점수 계산 및 출력
-final_score = calculate_final_score(correct_answers)
-print("최종 점수 : {}".format(final_score))
+# MongoDB에 연결하고 지정된 데이터베이스와 컬렉션에 연결하는 함수를 정의
+def connect_to_mongodb(database_name, collection_name):
+    # MongoClient를 사용하여 로컬호스트의 MongoDB에 연결
+    client = MongoClient('mongodb://localhost:27017/')
+    # 지정된 데이터베이스를 선택
+    database = client[database_name]
+    # 지정된 컬렉션을 선택
+    collection = database[collection_name]
+    # 컬렉션 변수를 반환
+    return collection
+
+# 퀴즈 데이터를 MongoDB에 삽입하는 함수를 정의
+def insert_quiz_data(collection, quiz_list):
+    # 퀴즈 리스트의 각 퀴즈에 대해
+    for quiz in quiz_list:
+        # 동일한 질문을 가진 문서가 존재하지 않는 경우에만
+        if collection.count_documents({"question": quiz["question"]}) == 0:
+            # 퀴즈를 컬렉션에 삽입
+            collection.insert_one(quiz)
+
+# MongoDB에 연결하고 지정된 데이터베이스와 컬렉션에 연결
+collection = connect_to_mongodb(database_name, collection_name)
+
+# 삽입할 퀴즈 데이터를 정의
+quiz_list = [
+    {
+        "question": "Python의 생성자 함수 이름은 무엇인가요?",
+        "choices": ["__init__", "__main__", "__str__", "__del__"],
+        "answer": "__init__",
+        "answer_number": 1,
+        "score": 20
+    },
+    {
+        "question": "Python에서 'Hello, World!'를 출력하는 코드는 무엇인가요?",
+        "choices": ["print('Hello, World!')", "console.log('Hello, World!')", "printf('Hello, World!')", "echo 'Hello, World!'"],
+        "answer": "print('Hello, World!')",
+        "answer_number": 1,
+        "score": 20
+    },
+    {
+        "question": "Python의 주석을 나타내는 기호는 무엇인가요?",
+        "choices": ["//", "/* */", "#", "--"],
+        "answer": "#",
+        "answer_number": 3,
+        "score": 20
+    },
+    {
+        "question": "Python에서 리스트의 길이를 반환하는 함수는 무엇인가요?",
+        "choices": ["size()", "length()", "len()", "sizeof()"],
+        "answer": "len()",
+        "answer_number": 3,
+        "score": 20
+    },
+    {
+        "question": "Python에서 문자열을 숫자로 변환하는 함수는 무엇인가요?",
+        "choices": ["str()", "int()", "char()", "float()"],
+        "answer": "int()",
+        "answer_number": 2,
+        "score": 20
+    }
+]
+
+# 퀴즈 데이터를 MongoDB에 삽입
+insert_quiz_data(collection, quiz_list)
