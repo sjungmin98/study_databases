fc5d297277fe60f55504798de9d17cb7e63cf8c2
sjungmin98 <shc980@gmail.com>
2023-12-14 11:26:56 +0900

    1111

--- docs/quests/mongo_insert_functions.py
+++ docs/quests/mongo_insert_functions.py
@@ -4,12 +4,12 @@
 #     {"name": "Orange", "color": "Orange", "origin": "Southeast Asia"},
 #     {"name": "Grapes", "color": "Purple", "origin": "Europe"}]
 
-from pymongo import MongoClient
-
 # Connect to MongoDB
-mongoclient = MongoClient("mongodb://localhost:27017/")
-database = mongoclient["local"]  # 로컬로 연결한거 변수로 받음
-collection = database["fruits"]  # 로컬안에 있는 fruit docutment 변수로 받음
+def connect_mongo(link, database_name, collection_name): 
+    from pymongo import MongoClient
+    mongoclient = MongoClient(link)
+    database = mongoclient[database_name]  # 로컬로 연결한거 변수로 받음
+    return database[collection_name]  # 로컬안에 있는 fruit docutment 변수로 받음
 
 def insert_fruit(fruit):
     collection.insert_one(fruit)
@@ -21,9 +21,10 @@ fruit_info_list = [
     {"name": "Orange", "color": "Orange", "origin": "Southeast Asia"},
     {"name": "Grapes", "color": "Purple", "origin": "Europe"}]
 
+collection = connect_mongo("mongodb://localhost:27017", "local", "fruits")
 
 # 각각의 정보를 mongoDB로 insert
-for fruit in fruit_info_list:
-    insert_fruit(fruit)
+for fruit in [0, 1, 2, 3]:
+    fruit_list = fruit_info_list[fruit]
+    insert_fruit(fruit_list)
 pass
-    
