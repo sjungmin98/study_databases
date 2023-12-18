4abf7d64282809aeaebc73f4ecad2823376ad885
sjungmin98 <shc980@gmail.com>
2023-12-18 14:40:41 +0900

    ALLSET

--- docs/quests/todolist_functions.py
+++ docs/quests/todolist_functions.py
@@ -1,74 +1,107 @@
-# MongoClient 모듈을 import
 from pymongo import MongoClient
 
-# MongoDB에 연결하고 지정된 데이터베이스와 컬렉션에 연결하는 함수를 정의
-def connect_to_mongodb(database_name, collection_name):
-    # MongoClient를 사용하여 로컬호스트의 MongoDB에 연결
+def connect_and_select():
+    # MongoDB에 연결하고 컬렉션들을 선택하는 함수
     client = MongoClient('mongodb://localhost:27017/')
-    # 지정된 데이터베이스를 선택
-    database = client[database_name]
-    # 지정된 컬렉션을 선택
-    collection = database[collection_name]
-    # 컬렉션 변수를 반환
-    return collection
+    database = client['local']
+    todos_collection = database['todos_list']  
+    part_collection = database['participants']  
+    part_todos_collection = database['participants_todos']  
+    return todos_collection, part_collection, part_todos_collection
 
-# MongoDB에서 문제와 선택지를 가져와서 사용자에게 문제를 출제하고 답을 받는 함수
-def question_choices_from_mongodb(collection):
-    user_name = input("이름을 입력해 주세요 : ")  # 사용자 이름 입력 받기
+def init_todo_list(todos_collection, part_todos_collection):
+    # 참여자들의 할 일 목록을 초기화하는 함수
+    part_todos_collection.delete_many({}) 
+    todo_list = [todo['title'] for todo in todos_collection.find()]  # 'todos_list' 컬렉션의 모든 할 일의 제목을 리스트로 만듦
+    return todo_list
 
-    query = {}  # MongoDB에서 모든 문서를 선택하기 위한 빈 쿼리(딕셔너리) 생성
-    
-    # 필요한 필드만 선택하기 위한 프로젝션 생성
-    # 프로젝션 : 한 document 전체를 선택하는 것이 아니라, 특정 필요한 데이터를 선택하는 것
-    projection = {'title': 1, 'description': 1, 'answer_number': 1, '_id': 1}  
-                                                                            
-    # 쿼리와 프로젝션을 사용하여 MongoDB에서 데이터를 가져옴
-    result = collection.find(query, projection)
-    result = list(result)  # 결과를 리스트로 변환
+def get_participants(part_collection, name):
+    # 참여자 이름을 통해 MongoDB에서 해당 참여자 정보를 검색하는 함수
+    participant = part_collection.find_one({ "name": name })  # 이름이 일치하는 참여자를 찾음
+    if participant is None:
+        print("{}은(는) 참여자 목록에 없습니다.".format(name))
+    return participant
 
-
-
-
-
-
-
-
-def get_mixed_questions():
-    
-    list_mixed_questions = []
-
-    # input 기능 추가
-    for inputs in range(3):
-        question = input("question : ")
-
-        list_mixed_answers = []
-        
-        answer = input("answer : ")
-        list_mixed_answers.append(answer)
-        correct_index = input("correct_index : ")
-        score = input("score : ")
-
-        # 입력값을 딕셔너리로 저장
-        dict_mixed_questions = {
-            'question': question,
-            'answer': list_mixed_answers,
-            'correct_index': correct_index,
-            'score': score
+def update_participants(part_collection, name, title, status):
+    # 참여자의 설문 응답을 업데이트하는 함수
+    # "$addToSet" 연산자는 주어진 값을 배열 필드에 추가하는데, 값이 이미 배열에 존재하면 아무 작업도 수행하지 않음.
+    # 따라서 이 연산자를 사용하면, 중복 없이 설문 응답을 "Titles" 배열에 추가
+    part_collection.update_one(
+        { "name": name },
+        { 
+            "$addToSet": {
+                "Titles": {
+                    "Title": title,
+                    "status": status
+                }
+            }
         }
-        list_mixed_questions.append(dict_mixed_questions)
+    )
 
-    return list_mixed_questions
+def add_todos(todos_collection, todo_list):
+    # 'title'과 'description' 키를 가진 dictionary의 list를 받음
+    for todo in todo_list:
+        # 각 할 일에서 'title'과 'description'을 추출
+        title = todo['title']
+        description = todo['description']
+        # 만약 해당 'title'을 가진 할 일이 todos_collection에 없다면
+        if todos_collection.find_one({"title": title}) is None:
+            # 새로운 할 일을 todos_collection에 추가
+            todos_collection.insert_one({"title": title, "description": description})
 
-mixed_questions_data = get_mixed_questions()
+def add_participants(part_collection, participants):
+    # 'name' 키를 가진 dictionary의 list를 받음
+    for participant in participants:
+        # 각 참가자에서 'name'을 추출
+        name = participant['name']
+        # 만약 해당 'name'을 가진 참가자가 part_collection에 없다면
+        if part_collection.find_one({"name": name}) is None:
+            # 새로운 참가자를 part_collection에 추가
+            # 이 때 참가자는 아직 어떤 할 일도 가지고 있지 않으므로 "Titles"는 빈 리스트로 설정
+            part_collection.insert_one({"name": name, "Titles": []})
 
-def print_mixed_questions(list_mixed_questions):
+def reset_participants_titles(part_collection):
+    # 참여자들의 'Titles' 필드를 초기화하는 함수
+    part_collection.update_many({}, {"$set": {"Titles": []}})
+
+def run_todo_list():
+    todos_collection, part_collection, part_todos_collection = connect_and_select()
+    reset_participants_titles(part_collection)  # 참여자들의 'Titles' 필드 초기화
+    todo_list = init_todo_list(todos_collection, part_todos_collection)
+    name = input("Input Your Name: ")  
     
-    # 입력값 출력
-    for mixed_questions in list_mixed_questions:
-        print("\"question\" : {}".format(mixed_questions['question']))
-        print("\"answer\" : {}".format(mixed_questions['answer']))
-        print("\"correct_index\" : {}".format(mixed_questions['correct_index']))
-        print("\"score\" : {}".format(mixed_questions['score']))
+    participant = get_participants(part_collection, name)  
+    if participant is None:  
+        return  
 
-# 함수 호출
-print_mixed_questions(get_mixed_questions())
+    while True:
+        print("ToDo List 중 하나 선택하세요!")
+        i = 1
+        for todo in todo_list:  
+            print(str(i) + ". " + todo)
+            i += 1
+        title_index = int(input("Title 번호: ")) - 1  
+        status = input("Status: ")  
+
+        title = todo_list[title_index]  
+
+        # 참여자의 할 일과 설문 응답을 'participants_todos' 컬렉션에 추가
+        part_todos_collection.insert_one({
+            "name": name,
+            "Title": title,
+            "status": status,
+            "object_id": participant['_id']
+        })
+        update_participants(part_collection, name, title, status)  # 참여자의 설문 응답 업데이트
+
+        quit_input = input("종료 여부: ") 
+        print("------------------------")
+        if quit_input == 'x':  
+            print("프로그렘이 종료되었습니다.")
+            return  
+        elif quit_input == 'q':  
+            name = input("Input Your Name: ")  
+            participant = get_participants(part_collection, name) 
+            if participant is None:  
+                return 
+run_todo_list()
--- docs/quests/todolist_main.py
+++ docs/quests/todolist_main.py
@@ -1,17 +1,18 @@
-import solvingProblems_function
+import todolist_functions
+todos_collection, part_collection, part_todos_collection = todolist_functions.connect_and_select()
 
-# MongoDB에 연결
-database_name = 'local'
-collection_name = 'solvingproblem'
-collection = solvingProblems_function.connect_to_mongodb(database_name, collection_name)
-
-# MongoDB 컬렉션의 모든 문서를 삭제하여 초기화
-collection.delete_many({})
-
-# ToDo 리스트 생성
 todo_list = [
     {"title": "주간 보고서 작성", "description": "팀의 주간 성과와 진행 상황에 대한 보고서를 작성합니다."},
     {"title": "이메일 확인 및 응답", "description": "미처 확인하지 못한 이메일을 확인하고 필요한 이메일에 대해 응답합니다."},
     {"title": "회의 준비", "description": "다가오는 회의에 대해 준비합니다. 주제 연구, 발표 자료 준비 등이 포함될 수 있습니다."},
     {"title": "프로젝트 계획서 수정", "description": "현재 진행 중인 프로젝트의 계획서를 검토하고 필요한 부분을 수정합니다."},
-    {"title": "팀 멤버와의 1:1 면담", "description": "팀 멤버와 개별적으로 만나서 그들의 업무 진행 상황, 이슈, 우려사항 등을 논의합니다."},]
+    {"title": "팀 멤버와의 1:1 면담", "description": "팀 멤버와 개별적으로 만나서 그들의 업무 진행 상황, 이슈, 우려사항 등을 논의합니다."},
+]
+todolist_functions.add_todos(todos_collection, todo_list)
+
+participants = [
+    {"name": "참여자1"},
+    {"name": "참여자2"},
+    {"name": "참여자3"}
+]
+todolist_functions.add_participants(part_collection, participants)
